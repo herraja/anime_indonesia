c837fcbe846494ea7a27042c60211c40031f9e6c
herraja <darkquezt@gmail.com>
2023-02-28 02:31:52 +0700

    Membuat model database Artikel Berita pertama

--- artikel/admin.py
+++ artikel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Berita
 
 # Register your models here.
+class AdminBerita(admin.ModelAdmin):
+    readonly_fields = ['waktu_post' 'waktu_edit', 'slug']
+
+admin.site.register(Berita)
--- artikel/models.py
+++ artikel/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Berita(models.Model):
+    """
+    Bagian dari halaman Artikel. Konten yang disajikan berupa berita seputar
+    anime, manga, novel, musik dan budaya pop Jepang lainnya.
+    """
+    judul_berita = models.CharField(max_length=255)
+    isi_berita = models.TextField()
+    penulis = models.CharField(max_length=20, default="authors")
+    kategori = models.CharField(max_length=20)
+    waktu_post = models.DateTimeField(auto_now_add=True, editable=False)
+    waktu_edit = models.DateTimeField(auto_now=True, editable=False)
+    slug = models.SlugField(max_length=255, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.judul_berita)
+        super(Berita, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.id} - {self.judul_berita}"
