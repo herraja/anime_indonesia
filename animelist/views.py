3de6f6df9d35cd5581617f63938b7006521016d9
maulanawesome5 <maulanawesome5@gmail.com>
2023-03-05 05:50:41 +0700

    Membuat views, routing, dan template HTML

--- animelist/views.py
+++ animelist/views.py
@@ -1,3 +1,26 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        "Hero" : "Portal Komunitas Anime",
+        "Lead" : "Terupdate se-Indonesia",
+        "page" : "Daftar Anime",
+        "website" : "Anime Indonesia",
+    }
+    return render(request, "animelist/index.html", context)
+
+def search(request):
+    search_query = requests.GET.get("q", "")
+    response = requests.get(f"https://api.jikan.moe/v3/search/anime?q={search_query}")
+    results = response.json()["results"]
+    context = {
+        "Hero" : "Portal Komunitas Anime",
+        "Lead" : "Terupdate se-Indonesia",
+        "page" : "Daftar Anime",
+        "results" : results,
+        "search_query" : search_query,
+        "website" : "Anime Indonesia",
+    }
+    return render(request, "animelist/search_result.html", context)
