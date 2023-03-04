703b603168730f7f348b0701f6907caa6d4b294a
maulanawesome5 <maulanawesome5@gmail.com>
2023-03-05 04:55:08 +0700

    Membuat data model Anime dan melakukan migrasi

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Anime
+
 
 # Register your models here.
+class AnimeAdmin(admin.ModelAdmin):
+    readonly_fields = ["slug", "updated", "created"]
+
+
+admin.site.register(Anime, admin_class=AnimeAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+class Anime(models.Model):
+    mal_id = models.IntegerField(unique=True)
+    anime_title = models.CharField(max_length=255)
+    anime_score = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    airing_time = models.CharField(max_length=50, blank=True)
+    studio = models.CharField(max_length=50, blank=True)
+    slug = models.SlugField(max_length=255, editable=False)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.anime_title)
+        super(Anime, self).save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return f"{self.id}. {self.anime_title}"
+    
+    class Meta:
+        ordering = ["-updated"]
