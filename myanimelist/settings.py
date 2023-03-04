173878018996077a926ced6f485117a5d09c43a9
maulanawesome5 <maulanawesome5@gmail.com>
2023-03-05 04:53:16 +0700

    Menambahkan variabel REST_FRAMEWORK

--- myanimelist/settings.py
+++ myanimelist/settings.py
@@ -67,6 +67,18 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    # # Use Django's standard `django.contrib.auth` permissions,
+    # # or allow read-only access for unauthenticated users.
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
+
 ROOT_URLCONF = 'myanimelist.urls'
 
 TEMPLATES = [
